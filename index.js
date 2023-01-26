c32a152142d6375af369bbd5ab2977bfadc917b8
Noussair Bettioui <104898267+Nossa1@users.noreply.github.com>
2023-01-26 16:37:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let sideDiv = document.getElementsByClassName("sideDiv");
+let myLogo = document.querySelector("myLogo");
+
+function showMenu() {
+  myLogo.style.display = "none";
+  }/
+
+
+//filter: brightness(50%);
+
+//document.getElementsById("sideDivId").style.display = "block";*/
